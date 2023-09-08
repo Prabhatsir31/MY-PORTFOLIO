12b3a5def13532d374cbe456b86267bd299d678d
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2023-09-09 00:55:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+// Changing the text in home section
+let words = document.querySelectorAll(".word");
+words.forEach((word)=>{
+    let letters = word.textContent.split("");
+    word.textContent="";
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = ()=>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out";
+        },i * 80);
+    });
+    nextWord.style.opacity="1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+        letter.className = "letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        },340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1; 
+};
+
+changeText();
+setInterval(changeText,3000);
+
+// =================Skills Circular Bar===================== 
+$(function() {
+    $('.chart').easyPieChart({
+      size: 160,
+      barColor: "#17d3e6",
+      scaleLength: 0,
+      lineWidth: 15,
+      trackColor: "#373737",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+
+// =================toogle icon navbar===========================
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove('bx-x');
+    navlinks.classList.replace('open');
+}
+
+// =================Scroll section active links===========================
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+// =================Sticky navbar===========================    
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+
+// =================remove toggle icon and navbar click navbar link (scroll)===========================    
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('open');
+
+};
+// ==================Scroll reveal==========================    
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
